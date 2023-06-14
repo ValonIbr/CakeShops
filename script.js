62dfb22500c6a37ade34e5697d5562b1ed80f6d4
ledcom <valonibrahimi09@gmail.com>
2023-06-14 22:48:40 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector(".container"),
+    pwShowHide = document.querySelectorAll(".showHidePw"),
+    pwFields = document.querySelectorAll(".password"),
+    signUp = document.querySelector("signup-link"),
+    login = document.querySelector("login-link");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        pwFields.forEach(pwField => {
+            if (pwField.type === "password") {
+                pwField.type = "text";
+
+
+                pwShowHide.forEach(icon => {
+                    icon.classList.replace("fa-eye-slash", "fa-eye");
+                })
+            }
+            else {
+                pwField.type = "password";
+
+                pwShowHide.forEach(icon => {
+                    icon.classList.replace("fa-eye", "fa-eye-slash");
+                })
+            }
+        })
+    })
+})
+
